9466f30680a20fe6fe56dc79daa9c7d3d7a79297
ProfessorSean <professorsean3@gmail.com>
2021-04-15 22:47:56 -0500

    Commedn

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="Kasutamaiza",
+      version="1.0.0",
+      description="The bot for the UPCF",
+      license='MIT',
+      author="Matias Codesal",
+      authoer_email="seanje.lenox@gmail.com",
+      url='https://github.com/ProfessorSean/Kasutamaiza',
+      packages=['modules', 'upcfcardsearch']
+)
